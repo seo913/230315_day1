b1915aaff3ff887d3d13a9e4aab3c6b983413c7e
서재민 <seojaemin@seojaemin-ui-MacBookPro.local>
2023-03-21 18:03:42 +0900

    LAST SCSS, JS_DAY1

---
+++ JSday/jsday1/variables.js
@@ -0,0 +1,45 @@
+// let iAmProgrammer = true;
+// let myFavoriteDrikIszeroCoke = true;
+// let iHaveAThousandBitcoins =false;
+
+// if(iHaveAThousandBitcoins){
+//     console.log("You're a good person! ");
+// }else{
+//     console.log("You're a liar");
+// }
+
+// const TRUE = true;
+// const FALSE = false;
+
+// // && AND
+// console.log(TRUE && TRUE);
+// console.log(TRUE && FALSE);
+// console.log(FALSE && TRUE);
+// console.log(FALSE && FALSE);
+
+// // || OR
+// console.log(TRUE || TRUE);
+// console.log(TRUE || FALSE);
+// console.log(FALSE || TRUE);
+// console.log(FALSE || FALSE);
+
+// // ! NOT
+// console.log(!TRUE);
+// console.log(!FALSE);
+
+// var num = [1, 2, 3];
+
+// console.log(num[1]);
+
+    // var myProfile = {
+    //     name : 'seojaemin',
+    //     age : 29,
+    //     job: "haksaeng"
+    // }
+    // myProfile.age =25;
+
+    // console.log(myProfile.age);
+
+
+////////////////////////////////////////////////////////////
+
