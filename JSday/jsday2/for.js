ea6d7ac50097d56566e3a44f19c22219acef33a1
서재민 <seojaemin@seojaemin-ui-MacBookPro.local>
2023-03-22 15:49:15 +0900

    js for, function, if

---
+++ JSday/jsday2/for.js
@@ -0,0 +1,28 @@
+// for(let i = 0 ; i < 3 ; i++){   
+//     let star='';
+//     for(let j = 0 ; j < 5 ; j++){ 
+//         star += '*';
+//         // if(j < i){
+//         //     star += '*';
+//         // }
+//     }
+//     console.log(star);
+// }
+
+
+for(let a = 0 ; a < 10 ; a++){   
+    let star2='';
+    for(let b = 5 ; b > a ; b--){ 
+        star2 += '*';
+        // if(j < i){
+        //     star += '*';
+        // }
+    }
+    console.log(star2);
+}
+
+
+
+
+// i 줄 문단   i가 1번돌때 j가 10번돈다 그게 한세트
+// j 문단안에 있는 개수
---
+++ JSday/jsday2/if.js
@@ -0,0 +1,38 @@
+// const BLOCK = "블";
+// const BBLE = "쁠";
+// const CHAIN ="체";
+// const SCHOOL = "스";
+
+// let rightWord = BLOCK + CHAIN + SCHOOL;
+
+// if(rightWord === '블체스'){
+//     console.log('블체스 화이팅!');
+// }
+
+
+// const IMPROGRRER = true;
+// const IMBITCHANGAE = false;
+// const ILIKECOCA = true;
+
+
+// if(IMPROGRRER === false){
+//     console.log('yes');
+// }else{
+//     console.log('nono');
+
+// }
+
+let blockchainScore = 50;
+let frontendScore = 10;
+
+let totalScore = blockchainScore + frontendScore;
+
+if(totalScore > 78){
+    console.log('Great');
+}else if(totalScore > 50){
+    console.log('Good');
+}else if(totalScore > 30){
+    console.log('Try harder');
+}else{
+    console.log('Sad,,,');
+}
